99bbec005a88e7d15d5578e8b3d33ba31921f9f4
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-22 12:05:33 +0300

    Update ImagesGalleryItem.jsx

--- src/components/ImagesGalleryItem/ImagesGalleryItem.jsx
+++ src/components/ImagesGalleryItem/ImagesGalleryItem.jsx
@@ -1,42 +1,35 @@
+import { useState } from 'react';
 import { GalleryImage } from './ImagesGalleryItem.styled';
 import PropTypes from 'prop-types';
-import Modal from 'components/Modal';
-import { Component } from 'react';
-class ImagesGalleryItem extends Component {
-  static propTypes = {
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string,
-    tags: PropTypes.string.isRequired,
-  };
+import ImageModal from 'components/Modal';
+const ImagesGalleryItem = ({ webformatURL, tags, info }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  state = {
-    isModalOpen: false,
-  };
+  const closeModal = () => setIsModalOpen(false);
+  const openModal = () => setIsModalOpen(true);
 
-  openModal = () => this.setState({ isModalOpen: true });
-  closeModal = () => this.setState({ isModalOpen: false });
-  render() {
-    const { webformatURL, tags, info } = this.props;
-    const { isModalOpen } = this.state;
+  return (
+    <>
+      <GalleryImage
+        src={webformatURL}
+        alt={tags}
+        info={info}
+        onClick={openModal}
+      />
+      <ImageModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        largeImageUrl={info.largeImageUrl}
+        tags={tags}
+      />
+    </>
+  );
+};
 
-    return (
-      <>
-        <GalleryImage
-          src={webformatURL}
-          alt={tags}
-          info={info}
-          onClick={this.openModal}
-        />
-        {isModalOpen && (
-          <Modal
-            largeImageUrl={this.props.info.largeImageUrl}
-            tags={this.props.info.tags}
-            onClose={this.closeModal}
-          />
-        )}
-      </>
-    );
-  }
-}
+ImagesGalleryItem.propTypes = {
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string,
+  tags: PropTypes.string.isRequired,
+};
 
 export default ImagesGalleryItem;
